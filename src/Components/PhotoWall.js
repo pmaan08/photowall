9998e473a763a8c1903f4f8e2d397d864a990e73
pmann08 <partulmann@gmail.com>
2022-06-30 20:25:41 +0530

    Update - sort by timestamp id

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -12,7 +12,11 @@ function PhotoWall(props) {
             < Link className = "addIcon"  to="/AddPhoto" >  </Link>   
             {/*<button onClick = {props.onNavigate} className='addIcon'>  </button>*/}
             <div className= "photoGrid">
-                 {props.posts.map((post, index) => <Photo key={index} post = {post} onRemovePhoto = {props.onRemovePhoto} />) }
+                 {props.posts
+                 .sort(function(x,y) {
+                    return y.id - x.id
+                 })
+                 .map((post, index) => <Photo key={index} post = {post} onRemovePhoto = {props.onRemovePhoto} />) }
             </div>
             </div>        
 }
